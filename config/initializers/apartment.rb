cdcafce34a29bd6ac17d609cd782396aec5d970e
Abdul Waqas <abdulwaqas000@gmail.com>
2021-12-24 14:43:51 +0500

    adding www heroku in appartment.rb

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -111,6 +111,8 @@ end
 # }
 
 # Rails.application.config.middleware.use Apartment::Elevators::Domain
-# Rails.application.config.middleware.use Apartment::Elevators::Subdomain
+Rails.application.config.middleware.use Apartment::Elevators::Subdomain
 # Rails.application.config.middleware.use Apartment::Elevators::FirstSubdomain
 # Rails.application.config.middleware.use Apartment::Elevators::Host
+Apartment::Elevators::Subdomain.excluded_subdomains = ['heroku']
+Apartment::Elevators::Subdomain.excluded_subdomains = ['www']
