32a55583e634ecfbf2ca507f33416d8f7a08cb6c
Abdul Waqas <abdulwaqas000@gmail.com>
2021-12-30 15:30:10 +0500

    adding tenant page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
+Shop.destroy_all
 # Examples:
-Shop.create!(name: 'admin')
+# Shop.create!(name: 'admin')
 
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
